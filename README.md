67e77f3985e3fd5d68e55159235afafc0e929bc3
Igor Perfilov <6770145@gmail.com>
2024-05-12 07:05:43 +0300

    readme formatting

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-Internet radio player with Textual interface.
-To edit the radiostation list, modify STATIONLIST below.
-Not all stream types are working, unfortunately.
-pls/m3u/etc playlists not supported. Only mp3 streams accepted.
+Internet radio player with Textual interface.  
+To edit the radiostation list, modify STATIONLIST below.  
+Not all stream types are working, unfortunately.  
+pls/m3u/etc playlists not supported. Only mp3 streams accepted.  
+
 
 ![screenshot](screenshot.png)
 
-HOW TO RUN:
-run "pip install requriements.txt" (miniaudio, textual)
-run "python main.py"
 
-recommended Windows Terminal
+HOW TO RUN:  
+run "pip install requriements.txt" (miniaudio, textual)  
+run "python main.py"  
 
-TODO - next and previous buttons
+recommended Windows Terminal, but cmd is more or less ok.  
 
-TODO - histogram for the audio stream. Don't know how to start
-seems miniaudio has no such option? or something like  DecodedSoundFile?
-maybe from the system volume/mixer?
-for display, maybe sparkines?
+TODO - working next and previous buttons.  
 
-TODO - while no histogram, make 'press/event' indicator there
+TODO - histogram for the audio stream. Don't know how to start.  
+seems miniaudio has no such option? or something like  DecodedSoundFile?  
+maybe from the system volume/mixer?..
+for display, maybe sparkines?  
+
+TODO - while no histogram, make 'press/event' indicator there.  
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 """
-Internet radio player with Textual interface.
-To edit the radiostation list, modify STATIONLIST below.
-Not all stream types are working, unfortunately.
-pls/m3u/etc playlists not supported. Only mp3 streams accepted.
+Internet radio player with Textual interface.  
+To edit the radiostation list, modify STATIONLIST below.  
+Not all stream types are working, unfortunately.  
+pls/m3u/etc playlists not supported. Only mp3 streams accepted.  
 
-HOW TO RUN:
-run "pip install requriements.txt" (miniaudio, textual)
-run "python main.py"
+HOW TO RUN:  
+run "pip install requriements.txt" (miniaudio, textual)  
+run "python main.py"  
 
-recommended Windows Terminal or
+recommended Windows Terminal  
 
-TODO - next and previous buttons
+TODO - remove 'STATIC' text, make screenshot with actual song  
+TODO - next and previous buttons  
 
-TODO - histogram for the audio stream. Don't know how to start
-seems miniaudio has no such option? or something like  DecodedSoundFile?
-maybe from the system volume/mixer?
-for display, maybe sparkines?
+TODO - histogram for the audio stream. Don't know how to start  
+seems miniaudio has no such option? or something like  DecodedSoundFile?  
+maybe from the system volume/mixer?  
+for display, maybe sparkines?  
 
-TODO - while no histogram, make 'press/event' indicator there
+TODO - while no histogram, make 'press/event' indicator there  
 
 """
 
